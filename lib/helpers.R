5a765eb63e52e545999d76badfedce808590d16c
fwilhelm <franciscowilhelm@users.noreply.github.com>
2019-06-09 13:44:17 +0200

    minor update
    added functions from my github

--- lib/helpers.R
+++ lib/helpers.R
@@ -2,3 +2,12 @@ helper.function <- function()
 {
   return(1)
 }
+
+# R functions from my github
+source("https://raw.githubusercontent.com/franciscowilhelm/r-collection/master/gather_multilevel.R")
+source("https://raw.githubusercontent.com/franciscowilhelm/r-collection/master/scalecompute.R")
+source("https://raw.githubusercontent.com/franciscowilhelm/r-collection/master/modelstring.R")
+source("https://raw.githubusercontent.com/franciscowilhelm/r-collection/master/scalecompute_scoresonly.R")
+source("https://raw.githubusercontent.com/franciscowilhelm/r-collection/master/scoreItemsMulti.R")
+source("https://raw.githubusercontent.com/franciscowilhelm/r-collection/master/select_cormatrix.R")
+source("https://raw.githubusercontent.com/franciscowilhelm/r-collection/master/slideExtract.R")
